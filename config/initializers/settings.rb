2a4fcf71bced41fd1de491eb4f2bfb52ef6793e3
gssbzn <gssbzn@gmail.com>
2013-12-26 19:14:14 -0430

    heroku stuff

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,11 +1,14 @@
-$app_name = 'APP'
-$app_author = 'Gustavo Bazan'.html_safe
-$app_description = 'Webapp'.html_safe
-if Setting.table_exists?
-  setting = Setting.first
-  if setting
-    $app_name = setting.app_name
-    $app_author = setting.author
-    $app_description = setting.app_description
+begin
+  if ActiveRecord::Base.conected? && Setting.table_exists?
+    setting = Setting.first
+    if setting
+      $app_name = setting.app_name
+      $app_author = setting.author
+      $app_description = setting.app_description
+    end
   end
+rescue
+  $app_name = 'APP'
+  $app_author = 'Gustavo Bazan'.html_safe
+  $app_description = 'Webapp'.html_safe
 end
